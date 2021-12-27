b4b31feba556f0d63c111d298e0b3c6390054181
Jiro-XD <muhammadshahzadali@outlook.com>
2021-12-27 17:44:23 +0200

    cart functionalitites

--- EcommerceWebsite/views.py
+++ EcommerceWebsite/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from store.models import Product
 
-def home(requst):
+def home(request):
     products = Product.objects.all().filter(is_available=True)
 
     context = {
         'products': products,
     }
-    return render(requst, 'home.html', context)
+    return render(request, 'home.html', context)
---
+++ carts/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.cart, name='cart'),
+    path('add_cart/<int:product_id>/', views.add_cart, name='add_cart'),
+    path('reduce_cart/<int:product_id>/', views.reduce_cart, name='reduce_cart'),
+    path('remove_cartItem/<int:product_id>/', views.remove_cartItem, name='remove_cartItem'),
+]
