5d08212d642f3ac582be7a5e607c1f9d0f208878
dev-shahzadali <muhammadshahzadali@outlook.com>
2024-08-02 19:18:43 +0200

    managed requirements.txt file

--- EcommerceWebsite/settings.py
+++ EcommerceWebsite/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=True, cast=bool)
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
